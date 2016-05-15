68088f3cac74281a92c81747348887e3c340321b
Etienne A. Thevenot <etienne.thevenot@cea.fr>
2016-05-15 21:20:03 +0000

    CHANGES IN VERSION 1.4.0 ------------------------------
    BUG FIXED
    
        o stop messages generated when non-significant predictive or first orthogonal components are obtained
    
    git-svn-id: file:///home/git/hedgehog.fhcrc.org/bioconductor/branches/RELEASE_3_3/madman/Rpacks/ropls@117515 bc3139a8-67e5-0310-9ffc-ced21a209358

--- inst/unitTests/test_opls.R
+++ inst/unitTests/test_opls.R
@@ -85,6 +85,24 @@ test_PCA <- function() {
 
 }
 
+test_PCA_sacurine <- function() {
+
+    data(sacurine)
+
+    sac.pca <- opls(sacurine[["dataMatrix"]],
+                    printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getSummaryDF(sac.pca)["Total", "R2X(cum)"],
+                       0.501,
+                       tolerance = 1e-3)
+    sac.pcaSvd <- opls(sacurine[["dataMatrix"]],
+                       algoC = "svd",
+                       printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getScoreMN(sac.pcaSvd)[1, 1],
+                       -8.744009,
+                       tolerance = 1e-5)
+
+}
+
 test_PLS_single <- function() {
 
     data(cornell) ## see Tenenhaus, 1998
@@ -121,117 +139,8 @@ test_PLS_multiple <- function() {
 
 }
 
-test_sacurine_PCA <- function() {
 
-    data(sacurine)
-
-    sac.pca <- opls(sacurine[["dataMatrix"]],
-                    printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getSummaryDF(sac.pca)["Total", "R2X(cum)"],
-                       0.501,
-                       tolerance = 1e-3)
-    sac.pcaSvd <- opls(sacurine[["dataMatrix"]],
-                       algoC = "svd",
-                       printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getScoreMN(sac.pcaSvd)[1, 1],
-                       -8.744009,
-                       tolerance = 1e-5)
-
-}
-
-test_sacurine_PLSDA <- function() {
-
-    data(sacurine)
-
-    sac.plsda <- opls(sacurine[["dataMatrix"]],
-                      matrix(sacurine[["sampleMetadata"]][, "gender"], ncol = 1),
-                      permI = 0, printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getSummaryDF(sac.plsda)["Total", "Q2(cum)"],
-                       0.584,
-                       tolerance = 1e-3)
-    sac.plsda <- opls(sacurine[["dataMatrix"]],
-                      sacurine[["sampleMetadata"]][, "gender"],
-                      permI = 0, printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getVipVn(sac.plsda)[5],
-                       0.7034828,
-                       tolerance = 1e-7)
-
-    ## splitting the dataset between a reference and a test subsets
-
-    sac.plsdaCrv <- opls(sacurine[["dataMatrix"]],
-                         sacurine[["sampleMetadata"]][, "gender"],
-                         subset = setdiff(1:nrow(sacurine[["dataMatrix"]]), 1:10),
-                         permI = 0, printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getSummaryDF(sac.plsdaCrv)["Total", "RMSEP"],
-                       0.275,
-                       tolerance = 1e-3)
-    ## for subset = "odd"
-    ##     R2X(cum)  R2Y(cum)   Q2(cum)     RMSEE     RMSEP pre ort
-    ## h2 0.1802609 0.7666581 0.5618918 0.2446342 0.3422395   2   0
-
-}
-
-test_sacurine_OPLSDA <- function() {
-
-    data(sacurine)
-
-    sac.oplsda <- opls(sacurine[["dataMatrix"]],
-                       matrix(sacurine[["sampleMetadata"]][, "gender"],
-                              ncol = 1),
-                       predI = 1,
-                       orthoI = 1,
-                       permI = 0, printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getScoreMN(sac.oplsda, orthoL = TRUE)[1, 1],
-                       4.980604,
-                       tolerance = 1e-7)
-    ##     R2X(cum) R2Y(cum) Q2(cum) RMSEE pre ort
-    ## sum    0.185    0.668   0.554 0.289   1   1
-
-    checkEqualsNumeric(getVipVn(sac.oplsda)[2],
-                       1.551154,
-                       tolerance = 1e-6)
-
-    checkEqualsNumeric(getVipVn(sac.oplsda, orthoL = TRUE)[3],
-                       1.642173,
-                       tolerance = 1e-6)
-
-    ## permutation testing
-
-    sac.oplsdaPer <- opls(sacurine[["dataMatrix"]],
-                          sacurine[["sampleMetadata"]][, "gender"],
-                          predI = 1,
-                          orthoI = 1,
-                          permI = 10,
-                          printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(sac.oplsdaPer@suppLs[["permMN"]][1, 1],
-                       0.185,
-                       tolerance = 1e-3)
-
-}
-
-
-test_sacurine_PLSDA_pareto <- function() {
-
-    data(sacurine)
-
-    sac.plsdaPar <- opls(sacurine[["dataMatrix"]],
-                         matrix(sacurine[["sampleMetadata"]][, "gender"], ncol = 1),
-                         scaleC = "pareto",
-                         permI = 0, printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getSummaryDF(sac.plsdaPar)["Total", "Q2(cum)"],
-                       0.521,
-                       tolerance = 1e-3)
-    sac.plsdaPar <- opls(sacurine[["dataMatrix"]],
-                         sacurine[["sampleMetadata"]][, "gender"],
-                         scaleC = "pareto",
-                         permI = 0, printL = FALSE, plotL = FALSE)
-    checkEqualsNumeric(getVipVn(sac.plsdaPar)[5],
-                       0.7710519,
-                       tolerance = 1e-7)
-
-}
-
-test_sacurine_PLS_predict <- function() {
+test_PLS_predict_sacurine <- function() {
 
     data(sacurine)
 
@@ -275,23 +184,63 @@ test_sacurine_PLS_predict <- function() {
 
 }
 
-test_subset <- function() {
-    ## bug fixed following T. Souza remark
 
-    tiagoMN <- c(10, 11, 12, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 8, 10, 11, 10, 12, 10, 11, 10, 11, 10, 11, 11, 10, 11, 10, 11, 10, 11, 10, 12, 10, 5, 6, 5, 6, 7, 6, 5, 6, 5, 6, 15, 16, 15, 16, 15, 16, 15, 14, 15, 15, 16, 16, 16, 14, 16, 15, 16, 14, 16, 14, 2, 3, 2, 3, 22, 3, 4, 3, 2, 3, 3, 7, 3, 7, 3, 7, 4, 7, 3, 7, 3, 7, 3, 7, 3, 4, 3, 7, 3, 7, 2, 8, 2, 8, 20, 8, 2, 4, 2, 8, 7, 3, 7, 3, 4, 3, 7, 3, 7, 7, 3, 7, 3, 7, 3, 7, 3, 7, 6, 8, 25, 29, 25, 24, 25, 29, 25, 29, 25, 25, 23, 27, 23, 27, 23, 27, 23, 27, 23, 25, 20, 21, 22, 21, 20, 21, 20, 21, 20, 21)
-    dim(tiagoMN) <- c(20, 8)
-    dimnames(tiagoMN) <- list(1:20, letters[1:8])
-    tiagoFc <- rep(c("P", "F"), each = 10)
+test_PLSDA_sacurine <- function() {
 
-    opls(tiagoMN, tiagoFc, predI = 1, orthoI = 1, subset = "odd")
+    data(sacurine)
 
-    oplExc <- opls(tiagoMN, tiagoFc, subset = seq(1, 20, by = 2))
-    checkException(plot(oplExc, parLabVc = paste0("s", seq(1, 20, by = 2))),
-                   silent = TRUE)
+    sac.plsda <- opls(sacurine[["dataMatrix"]],
+                      matrix(sacurine[["sampleMetadata"]][, "gender"], ncol = 1),
+                      permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getSummaryDF(sac.plsda)["Total", "Q2(cum)"],
+                       0.584,
+                       tolerance = 1e-3)
+    sac.plsda <- opls(sacurine[["dataMatrix"]],
+                      sacurine[["sampleMetadata"]][, "gender"],
+                      permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getVipVn(sac.plsda)[5],
+                       0.7034828,
+                       tolerance = 1e-7)
+
+    ## splitting the dataset between a reference and a test subsets
+
+    sac.plsdaCrv <- opls(sacurine[["dataMatrix"]],
+                         sacurine[["sampleMetadata"]][, "gender"],
+                         subset = setdiff(1:nrow(sacurine[["dataMatrix"]]), 1:10),
+                         permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getSummaryDF(sac.plsdaCrv)["Total", "RMSEP"],
+                       0.275,
+                       tolerance = 1e-3)
+    ## for subset = "odd"
+    ##     R2X(cum)  R2Y(cum)   Q2(cum)     RMSEE     RMSEP pre ort
+    ## h2 0.1802609 0.7666581 0.5618918 0.2446342 0.3422395   2   0
 
 }
 
-test_plsda_multiclass <- function() {
+
+test_PLSDA_sacurine_pareto <- function() {
+
+    data(sacurine)
+
+    sac.plsdaPar <- opls(sacurine[["dataMatrix"]],
+                         matrix(sacurine[["sampleMetadata"]][, "gender"], ncol = 1),
+                         scaleC = "pareto",
+                         permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getSummaryDF(sac.plsdaPar)["Total", "Q2(cum)"],
+                       0.521,
+                       tolerance = 1e-3)
+    sac.plsdaPar <- opls(sacurine[["dataMatrix"]],
+                         sacurine[["sampleMetadata"]][, "gender"],
+                         scaleC = "pareto",
+                         permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getVipVn(sac.plsdaPar)[5],
+                       0.7710519,
+                       tolerance = 1e-7)
+
+}
+
+
+test_PLSDA_multiclass <- function() {
 
     data(sacurine)
     ageVn <- sacurine[["sampleMetadata"]][, "age"]
@@ -330,3 +279,115 @@ test_plsda_multiclass <- function() {
                        tolerance = 1e-3)
 
 }
+
+
+test_OPLSDA_sacurine <- function() {
+
+    data(sacurine)
+
+    sac.oplsda <- opls(sacurine[["dataMatrix"]],
+                       matrix(sacurine[["sampleMetadata"]][, "gender"],
+                              ncol = 1),
+                       predI = 1,
+                       orthoI = 1,
+                       permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getScoreMN(sac.oplsda, orthoL = TRUE)[1, 1],
+                       4.980604,
+                       tolerance = 1e-7)
+    ##     R2X(cum) R2Y(cum) Q2(cum) RMSEE pre ort
+    ## sum    0.185    0.668   0.554 0.289   1   1
+
+    checkEqualsNumeric(getVipVn(sac.oplsda)[2],
+                       1.551154,
+                       tolerance = 1e-6)
+
+    checkEqualsNumeric(getVipVn(sac.oplsda, orthoL = TRUE)[3],
+                       1.642173,
+                       tolerance = 1e-6)
+
+    ## permutation testing
+
+    sac.oplsdaPer <- opls(sacurine[["dataMatrix"]],
+                          sacurine[["sampleMetadata"]][, "gender"],
+                          predI = 1,
+                          orthoI = 1,
+                          permI = 10,
+                          printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(sac.oplsdaPer@suppLs[["permMN"]][1, 1],
+                       0.185,
+                       tolerance = 1e-3)
+
+    ## number of components
+
+    sac.oplsda <- opls(sacurine[["dataMatrix"]],
+                       matrix(sacurine[["sampleMetadata"]][, "gender"],
+                              ncol = 1),
+                       predI = 1,
+                       orthoI = NA,
+                       permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getSummaryDF(sac.oplsda)[, "ort"],
+                       2,
+                       tolerance = 1e-3)
+
+    sac.oplsda <- opls(sacurine[["dataMatrix"]],
+                       matrix(sacurine[["sampleMetadata"]][, "age"],
+                              ncol = 1),
+                       predI = 1,
+                       orthoI = NA,
+                       permI = 0, printL = FALSE, plotL = FALSE)
+    checkEqualsNumeric(getSummaryDF(sac.oplsda)[, "ort"],
+                       1,
+                       tolerance = 1e-3)
+
+}
+
+
+test_OPLSDA_subset <- function() {
+    ## bug fixed following T. Souza remark
+
+    tiagoMN <- c(10, 11, 12, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 11, 10, 8, 10, 11, 10, 12, 10, 11, 10, 11, 10, 11, 11, 10, 11, 10, 11, 10, 11, 10, 12, 10, 5, 6, 5, 6, 7, 6, 5, 6, 5, 6, 15, 16, 15, 16, 15, 16, 15, 14, 15, 15, 16, 16, 16, 14, 16, 15, 16, 14, 16, 14, 2, 3, 2, 3, 22, 3, 4, 3, 2, 3, 3, 7, 3, 7, 3, 7, 4, 7, 3, 7, 3, 7, 3, 7, 3, 4, 3, 7, 3, 7, 2, 8, 2, 8, 20, 8, 2, 4, 2, 8, 7, 3, 7, 3, 4, 3, 7, 3, 7, 7, 3, 7, 3, 7, 3, 7, 3, 7, 6, 8, 25, 29, 25, 24, 25, 29, 25, 29, 25, 25, 23, 27, 23, 27, 23, 27, 23, 27, 23, 25, 20, 21, 22, 21, 20, 21, 20, 21, 20, 21)
+    dim(tiagoMN) <- c(20, 8)
+    dimnames(tiagoMN) <- list(1:20, letters[1:8])
+    tiagoFc <- rep(c("P", "F"), each = 10)
+
+    opls(tiagoMN, tiagoFc, predI = 1, orthoI = 1, subset = "odd")
+
+    oplExc <- opls(tiagoMN, tiagoFc, subset = seq(1, 20, by = 2))
+    checkException(plot(oplExc, parLabVc = paste0("s", seq(1, 20, by = 2))),
+                   silent = TRUE)
+
+}
+
+
+test_OPLSDA_ns <- function() {
+
+    datMN <- c(76043, 412165, 44943, 27242, 436566, 173175, 242549, 57066, 559869, 3732, 339188, 471368, 262271, 127285, 451270, 212500, 79673, NA, 891129, 43907, 30689, 6877586, 52217, 3158, 10789748, 229568, 4763576, 3878773, 976436, 831937, 608298, 1605075, 72021, 442510, 1107705, 1464339, 31250, 2724553, 72900, 32742, 47259, 544877, 60885, 34582, 529874, 168264, 176500, 76457, 610110, 16262, 279156, 524468, 451573, 591487, 433529, 161069, 214392, 13781, 1580343, 39315, 357351, 1030464, 301983, 67604, 306862, 1028110, 1530493, 270027, 1378535, 289677, 808334, 1132813, 871209, 895435, 715190, 1563158, 784738, 146195, 994336, 239030, 483755, 579287, 1132413, 157113, 1577570, 1469735, 1085454, 477909, 814755, 245417, 610681, 763706, 2406336, 827531, 992508, 569605, 355321, 150259, 1334200, 271010, 2644620, 727587, 1661412, 619181, 136278, 2755434, 593863, 837865, 3526136, 2003278, 1608814, 3446611, 1941527, 113937, 3132404, 2893445, 2092753, 1034666, 1517319, 841661, 250551, 1046138, 456162, 159386, 1013302, 808657, 614370, 250403, 768004, 242085, 504108, 1014041, 1362408, 1057660, 1110050, 566050, 411886, 142233, 1992420, 284775, 560002, 771533, 575790, 392284, 888498, 785428, 645785, 591569, 960658, 910201, 639437, 1092885, 1409045, 2292023, 1246459, 1945577, 710519, 773384, 1061418, 622898, 34236, 58249, 85944, NA, 342102, 129886, 175800, 13154, 230242, NA, 440223, 315368, 10657, 419508, 48673, 28361, 514579, 23108, 867108, 73831, 1252089, 2547452, 905408, 371059, 4983588, 5140022, 2658555, 814523, 2558923, 859466, 4184204, 3865723, 3236644, 2615560, 3820724, 3577833, 2295288, 625924, 7517724, 1341900, 2569205, 26023086, 1604999, 430453, 8103558, 26222916, 257139, 675754, 59906109, 263055, 31151730, 18648127, 14989438, 1554658, 20249262, 5588731, 871010, 15920, 9120781, 44276, 747080, 13420742, 595872, 1172376, 7172632, 3143654, 4059767, 1433702, 5593888, 5402629, 2477288, 3346077, 4230072, 7621236, 8960828, 10335722, 7037373, 1574738, 3359238, 2540044, 374028, 1144386, 539206, 178517, 1046190, 959381, 605191, 310260, 1253319, 477259, 477995, 825691, 1157093, 1089284, 1411802, 1020206, 782673, 346761, 1824553, 387811, 53304, 319783, 280560, 85009, 1333877, 556003, 590779, 209285, 342532, 198512, 569970, 525240, 246282, 1140422, 542345, 1171008, 827723, 222953, 438839, 85554, 368600, 616555, 94936, 622468, 180988, 293988, 352855, 767894, 268331, 167246, 310918, 1248919, 577184, 10985, 335711, 403815, 80614, 63393, 454489, 616061)
+
+    dim(datMN) <- c(20, 15)
+
+    ageVn <- c(41, 41, 52, 24, 55, 46, 61, 53, 23, 50, 33, 48, 42, 35, 26, 35, 60, 42, 31, 27)
+
+    checkException(opls(datMN,
+                        ageVn,
+                        predI = 1,
+                        orthoI = NA,
+                        permI = 0,
+                        printL = FALSE,
+                        plotL = FALSE),
+                   silent = TRUE)
+
+    checkException(opls(datMN,
+                        ageVn,
+                        predI = 1,
+                        orthoI = NA,
+                        permI = 0,
+                        scaleC = "pareto",
+                        printL = FALSE,
+                        plotL = FALSE),
+                   silent = TRUE)
+
+}
+
+
+
+
